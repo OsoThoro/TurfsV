8da1254f16c06821b90f264d86f286b8bd5061d6
Oso <87783701+OsoThoro@users.noreply.github.com>
2024-10-12 22:38:36 -0400

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,5 @@
+Locales['en'] = {
+    ['turf_captured'] = 'You have successfully captured the turf!',
+    ['turf_contested'] = 'The turf is being contested!',
+    ['gang_own_turf'] = 'This turf is already controlled by your gang!',
+}
